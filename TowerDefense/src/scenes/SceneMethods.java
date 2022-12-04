b9bfbe9d375b2d88623ecef62e80b57d3ddc1023
huythanh1324 <119152193+huythanh1324@users.noreply.github.com>
2022-12-04 11:36:08 +0700

    Add files via upload

---
+++ TowerDefense/src/scenes/SceneMethods.java
@@ -0,0 +1,20 @@
+package scenes;
+
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+
+public interface SceneMethods {
+
+	public void render(Graphics g);
+
+	public void mouseClicked(int x, int y);
+
+	public void mouseMoved(int x, int y);
+
+	public void mousePressed(int x, int y);
+
+	public void mouseReleased(int x, int y);
+	
+	public void mouseDragged(int x, int y);
+
+}
